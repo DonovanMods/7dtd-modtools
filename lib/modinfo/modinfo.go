caf05435eec574cc8617575c85480f787d8a146b
Donovan C. Young <dyoung522@gmail.com>
2024-12-30 19:02:15 -0500

    Add copyright info to file headers

--- lib/modinfo/modinfo.go
+++ lib/modinfo/modinfo.go
@@ -1,3 +1,19 @@
+/*
+Copyright © 2024 Donovan C. Young <dyoung522@gmail.com>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
 package modinfo
 
 import (
@@ -22,17 +38,6 @@ type ModInfo struct {
 	Website     xmlValue `xml:"Website"`
 }
 
-func New(modletName string) ModInfo {
-	return ModInfo{
-		Name:        xmlValue{Value: modletName},
-		DisplayName: xmlValue{Value: "My New Modlet"},
-		Description: xmlValue{Value: fmt.Sprintf("This is the description for %s -- please change it", modletName)},
-		Author:      xmlValue{Value: "Unknown"},
-		Version:     xmlValue{Value: "0.1.0", Compat: "V1"},
-		Website:     xmlValue{Value: "https://example.org"},
-	}
-}
-
 func (M ModInfo) XML() (string, error) {
 	xml, err := XML.MarshalIndent(M, "", "  ")
 	if err != nil {
@@ -42,4 +47,15 @@ func (M ModInfo) XML() (string, error) {
 	xml = []byte(xmltools.RemoveClosingXMLTags(string(xml)))
 
 	return string(xml), nil
+}
+
+func New(modletName string) ModInfo {
+	return ModInfo{
+		Name:        xmlValue{Value: modletName},
+		DisplayName: xmlValue{Value: "My New Modlet"},
+		Description: xmlValue{Value: fmt.Sprintf("This is the description for %s -- please change it", modletName)},
+		Author:      xmlValue{Value: "Unknown"},
+		Version:     xmlValue{Value: "0.1.0", Compat: "V1"},
+		Website:     xmlValue{Value: "https://example.org"},
+	}
 }
